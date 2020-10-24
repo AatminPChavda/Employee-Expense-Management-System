60ea8b407256f41ccb692bf69f1d9ed054f52dba
AatminPChavda <aatminchavda@gmail.com>
2020-10-24 12:02:42 +0530

    EP_Request added

--- EEMS/src/Transaction/MP_Request.java
+++ EEMS/src/Transaction/MP_Request.java
@@ -14,12 +14,12 @@ import net.proteanit.sql.DbUtils;
  *
  * @author aatmin
  */
-public class P_Request extends javax.swing.JFrame {
+public class MP_Request extends javax.swing.JFrame {
 
     /**
      * Creates new form P_Request
      */
-    public P_Request() {
+    public MP_Request() {
         initComponents();
         PDisplay();
     }
@@ -162,20 +162,21 @@ public class P_Request extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(P_Request.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MP_Request.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(P_Request.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MP_Request.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(P_Request.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MP_Request.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(P_Request.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MP_Request.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new P_Request().setVisible(true);
+                new MP_Request().setVisible(true);
             }
         });
     }
